c443dddbb6c2089cc2aa0b078cbf944097f0103d
System Administrator <root@hero.lan>
2024-01-07 16:50:50 +0800

    feat: http demo

--- demo/http-client/demo.js
+++ demo/http-client/demo.js
@@ -1,9 +1,7 @@
+//API Address:  https://axios-http.com/docs/api_intro
 const axios = require('axios');
-
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
+    baseURL: 'http://localhost:3000', timeout: 1000, headers: {'X-Custom-Header': 'foobar'}
 });
 
 // instance.get('/').then(function (response) {
@@ -11,8 +9,7 @@ const instance = axios.create({
 // });
 
 instance.post('/users/do/sign', {
-    username: 'aaa',
-    email: 'bbb'
+    username: 'aaa', email: 'bbb'
 }).then(function (response) {
     console.log(response.data);
 });
